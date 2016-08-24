ad1d9be5dd7c33b07f45f58eba6ca8938eac512c
Guilherme Semissatto <semissatto@gmail.com>
2016-08-24 09:56:16 -0300

    Update telnetData.py

--- telnetData.py
+++ telnetData.py
@@ -4,6 +4,6 @@ print "Qual porta para comunicacao Telnet?"
 porta = raw_input()
 print "Qual o range (r) selecionado? Opcoes: 0, 1, 2, 3, 4, 5, 6 e 7"
 ranged = raw_input()
-print "Qual o numero de samples (r) selecionado? Opcoes: 1...4095"
+print "Qual o numero de samples (n) selecionado? Opcoes: 1...4095"
 n = raw_input()
-print "O ip e : $ip . A porta e $porta . O range e $range . O numero de samples e $n "
+print "O ip e : %d . A porta e %s . O range e %s . O numero de samples e %s " %( ip, porta, ranged, n)
