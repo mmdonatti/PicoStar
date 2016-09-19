255e483a9f6c7c2b9c294120866b59b16d04d27f
semissatto2 <semissatto@gmail.com>
2016-09-19 10:34:57 -0300

    New

--- telnetDataGraph.py
+++ telnetDataGraph.py
@@ -111,4 +111,4 @@ while True:
 		print "%f	nA	%f	nA	%f	nA	%f	nA" % (ch_treated[0], ch_treated[1], ch_treated[2], ch_treated[3])
 		if log_flag == 's':
 			file.write(str(datetime.datetime.now())+"	"+str.format("{0:.9f}",ch_treated[0])+"	nA	"+str.format("{0:.9f}",ch_treated[1])+"	nA	"+str.format("{0:.9f}",ch_treated[2])+"	nA	"+str.format("{0:.9f}",ch_treated[3])+" nA\n" ) 
-			p1 = win.addPlot(title="Basic array plotting", y=ch_treate[0])
+		p1 = win.addPlot(title="Basic array plotting", y=ch_treated[0])
