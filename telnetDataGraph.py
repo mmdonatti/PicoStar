3240fc846546a05514a68dc9ac5a6da0e0cf5d56
Guilherme Semissatto <semissatto@gmail.com>
2016-09-19 13:26:22 -0300

    Update telnetDataGraph.py

--- telnetDataGraph.py
+++ telnetDataGraph.py
@@ -98,7 +98,7 @@ while True:
 				ch_treated[i] = 0
 		print "%f	nA	%f	nA	%f	nA	%f	nA" % (ch_treated[0], ch_treated[1], ch_treated[2], ch_treated[3])
 		#print  ch0_treated_saved
-		print auxiliar2
+		print tempo
 		if log_flag == 's':
 			file.write(str(datetime.datetime.now())+"	"+str.format("{0:.9f}",ch_treated[0])+"	nA	"+str.format("{0:.9f}",ch_treated[1])+"	nA	"+str.format("{0:.9f}",ch_treated[2])+"	nA	"+str.format("{0:.9f}",ch_treated[3])+" nA\n" ) 
 		pw.plot(tempo, ch0_treated_saved, clear=True)
